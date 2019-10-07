eb43816d210bcc263bec99d3ee228d6b064e9c70
Daelamar <redfess@yandex.ru>
2019-10-07 22:20:08 +0300

    JS
    -Доделал слайдер

--- js/slider.js
+++ js/slider.js
@@ -5,8 +5,8 @@ var slider = document.querySelector('.slider');
 var sliderItemLeft = slider.querySelector('.slider__left');
 var sliderItemRight = slider.querySelector('.slider__right');
 
-var sliderArrowLeft = slider.querySelector('.slider__item slider__left');
-var sliderArrowRight = slider.querySelector('.slider__item slider__right');
+var sliderArrowLeft = slider.querySelector('.slider__arrow--left');
+var sliderArrowRight = slider.querySelector('.slider__arrow--right');
 
 var sliderDotLeft = slider.querySelector('.slider__dot--left');
 var sliderDotRight = slider.querySelector('.slider__dot--right');
@@ -31,34 +31,31 @@ sliderDotRight.addEventListener('click', function (evt) {
   sliderSpanLeft.classList.remove('slider__span--active');
 });
 
-
-// var getSlideItem = function () {
-//   if (sliderItemLeft.classList.contains('slider__item--active')) {
-//     sliderItemRight.classList.add('slider__item--active');
-//     sliderItemLeft.classList.remove('slider__item--active');
-//   } else if (sliderItemRight.classList.contains('slider__item--active')) {
-//     sliderItemLeft.classList.add('slider__item--active');
-//     sliderItemRight.classList.remove('slider__item--active');
-//   }
-// };
-//
-// sliderArrowLeft.addEventListener('click', function (evt) {
-//   evt.preventDefault();
-//   if (sliderItemLeft.classList.contains('slider__item--active')) {
-//     sliderItemRight.classList.add('slider__item--active');
-//     sliderItemLeft.classList.remove('slider__item--active');
-//   } else if (sliderItemRight.classList.contains('slider__item--active')) {
-//     sliderItemLeft.classList.add('slider__item--active');
-//     sliderItemRight.classList.remove('slider__item--active');
-//   }
-// });
-// sliderArrowRight.addEventListener('click', function (evt) {
-//   evt.preventDefault();
-//   if (sliderItemLeft.classList.contains('slider__item--active')) {
-//     sliderItemRight.classList.add('slider__item--active');
-//     sliderItemLeft.classList.remove('slider__item--active');
-//   } else if (sliderItemRight.classList.contains('slider__item--active')) {
-//     sliderItemLeft.classList.add('slider__item--active');
-//     sliderItemRight.classList.remove('slider__item--active');
-//   }
-// });
+sliderArrowLeft.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (sliderItemLeft.classList.contains('slider__item--active')) {
+    sliderItemRight.classList.add('slider__item--active');
+    sliderItemLeft.classList.remove('slider__item--active');
+    sliderSpanRight.classList.add('slider__span--active');
+    sliderSpanLeft.classList.remove('slider__span--active');
+  } else if (sliderItemRight.classList.contains('slider__item--active')) {
+    sliderItemLeft.classList.add('slider__item--active');
+    sliderItemRight.classList.remove('slider__item--active');
+    sliderSpanLeft.classList.add('slider__span--active');
+    sliderSpanRight.classList.remove('slider__span--active');
+  }
+});
+sliderArrowRight.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (sliderItemLeft.classList.contains('slider__item--active')) {
+    sliderItemRight.classList.add('slider__item--active');
+    sliderItemLeft.classList.remove('slider__item--active');
+    sliderSpanRight.classList.add('slider__span--active');
+    sliderSpanLeft.classList.remove('slider__span--active');
+  } else if (sliderItemRight.classList.contains('slider__item--active')) {
+    sliderItemLeft.classList.add('slider__item--active');
+    sliderItemRight.classList.remove('slider__item--active');
+    sliderSpanLeft.classList.add('slider__span--active');
+    sliderSpanRight.classList.remove('slider__span--active');
+  }
+});
