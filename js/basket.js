c45e8d0470f95ccdcca1d68eb630164185c34039
Daelamar <redfess@yandex.ru>
2019-10-08 17:40:44 +0300

    Предзащита
    JS - карта
    Микроправки наполнения контентом

--- js/basket.js
+++ js/basket.js
@@ -1,39 +1,39 @@
 'use strict';
 
 var buyBtnArr = document.querySelectorAll('.product-cart__button--buy');
-var popup = document.querySelector('.basket-popup');
-var close_btn = popup.querySelector('.basket-popup__close');
-var continueBtn = popup.querySelector('.basket-popup__continue');
+var basket = document.querySelector('.basket-popup');
+var close_basket = basket.querySelector('.basket-popup__close');
+var continueBtn = basket.querySelector('.basket-popup__continue');
 
 
-var getPopupBlock = function () {
+var getBasketBlock = function () {
   var buyBtn;
   for (var i = 0; i < buyBtnArr.length; i++) {
     buyBtn = buyBtnArr[i];
     buyBtn.addEventListener('click', function (evt) {
       evt.preventDefault();
-      popup.classList.add('show-popup');
+      basket.classList.add('show-popup');
     });
   }
 };
 
-close_btn.addEventListener('click', function (evt) {
+close_basket.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popup.classList.remove('show-popup');
+  basket.classList.remove('show-popup');
 });
 
 continueBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popup.classList.remove('show-popup');
+  basket.classList.remove('show-popup');
 });
 
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains('show-popup')) {
-      popup.classList.remove('show-popup');
+    if (basket.classList.contains('show-popup')) {
+      basket.classList.remove('show-popup');
     }
   }
 });
 
-getPopupBlock();
+getBasketBlock();
